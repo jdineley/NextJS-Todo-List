47e6783fc9728de89faf8eb41d1d81cb3d73e794
James Dineley <jdineley@hotmail.com>
2023-12-19 11:40:37 +0000

    Setup mysql & prisma orm

---
+++ prisma/migrations/20231219112801_create_todo/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Todo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `status` ENUM('IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'IN_PROGRESS',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
